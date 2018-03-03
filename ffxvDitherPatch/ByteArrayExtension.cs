9f0908a16305af7e3ffbf95ddd393432a369a397
Niklas K <drdaxxy@googlemail.com>
2018-03-03 19:54:34 +0100

    remove old Locate code

--- ffxvDitherPatch/ByteArrayExtension.cs
+++ ffxvDitherPatch/ByteArrayExtension.cs
@@ -9,8 +9,6 @@ namespace ffxvDitherPatch
 {
     static class ByteArrayExtension
     {
-        // https://stackoverflow.com/a/283648
-
         public static int[] SigScan(this byte[] haystack, string pattern)
         {
             List<int> matches = new List<int>();
@@ -50,47 +48,6 @@ namespace ffxvDitherPatch
             }
 
             return matches.ToArray();
-        }
-
-        static readonly int[] Empty = new int[0];
-
-        public static int[] Locate(this byte[] self, byte[] candidate)
-        {
-            if (IsEmptyLocate(self, candidate))
-                return Empty;
-
-            var list = new List<int>();
-
-            for (int i = 0; i < self.Length; i++)
-            {
-                if (!IsMatch(self, i, candidate))
-                    continue;
-
-                list.Add(i);
-            }
-
-            return list.Count == 0 ? Empty : list.ToArray();
-        }
-
-        static bool IsMatch(byte[] array, int position, byte[] candidate)
-        {
-            if (candidate.Length > (array.Length - position))
-                return false;
-
-            for (int i = 0; i < candidate.Length; i++)
-                if (array[position + i] != candidate[i])
-                    return false;
-
-            return true;
-        }
-
-        static bool IsEmptyLocate(byte[] array, byte[] candidate)
-        {
-            return array == null
-                || candidate == null
-                || array.Length == 0
-                || candidate.Length == 0
-                || candidate.Length > array.Length;
         }
     }
 }
